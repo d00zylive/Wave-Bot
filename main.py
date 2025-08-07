0cfea6c94d9ff694e04746c6f9b9322737fc49ef
Doozy <benjamin@starbirds.nl>
2025-08-07 23:35:32 +0200

    Added the first command group with a simple echo command that will change later. Also did the rest of the setup, like actually starting the bot and syncing.

--- main.py
+++ main.py
@@ -8,3 +8,20 @@ client = discord.Client(intents=intents)
 tree = app_commands.CommandTree(client)
 TOKEN = config("TOKEN")
 
+class preset(app_commands.Group):
+    def __init__(self):
+        super().__init__(name="preset", description="Commands related to presets")
+
+    @app_commands.command(name="generate", description="Replaces ::name:: with the preset correlated to the name")
+    @app_commands.describe(text="The text you want to add presets into")
+    async def generate(self, interaction: discord.Interaction, text: str) -> str:
+        await interaction.response.send_message(text)
+
+tree.add_command(preset())
+
+@client.event
+async def on_ready():
+    await tree.sync()
+    print("Ready!")
+
+client.run(TOKEN)
