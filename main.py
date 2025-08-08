8560c9fb6f6b39fe8f1a10269ba6b39043d27c67
Doozy <benjamin@starbirds.nl>
2025-08-08 12:29:19 +0200

    Generating texts with presets works now

--- main.py
+++ main.py
@@ -8,14 +8,23 @@ client = discord.Client(intents=intents)
 tree = app_commands.CommandTree(client)
 TOKEN = config("TOKEN")
 
+with open("presets.json", "r", encoding="utf8") as f:
+    presets = json.load(f)
+
 class preset(app_commands.Group):
     def __init__(self):
         super().__init__(name="preset", description="Commands related to presets")
 
-    @app_commands.command(name="generate", description="Replaces ::name:: with the preset correlated to the name")
+    @app_commands.command(name="generate", description="Replaces ::name:: with the preset correlated to said name")
     @app_commands.describe(text="The text you want to add presets into")
     async def generate(self, interaction: discord.Interaction, text: str) -> str:
-        await interaction.response.send_message(text)
+        localPresets: dict = presets[str(interaction.guild.id)]
+        response = ""
+        for substr in text.split(sep="::"):
+            if substr.lower() in localPresets.keys():
+                substr = localPresets[substr.lower()]
+            response += substr
+        await interaction.response.send_message(response)
 
 tree.add_command(preset())
 
